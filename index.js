75a6d69d08d5cf2738a0bab23ff2ae36ebae3892
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-04-28 21:30:28 +0300

    Second commit

--- index.js
+++ index.js
@@ -27,11 +27,34 @@ function outPut(N){
     if (N==1){
         return N
     } else {
-        return N.toString() + outPut(N - 1)
+        return N.toString()+outPut(N-1)
     }
 }
 console.log(outPut(5))
 //  
 
 // 4)
+function palidrom(str){
+    let lowerStr=str.replace(/\s+/g,'').toLowerCase()
+    if (lowerStr.length<=1){
+        return true
+    } 
+    if (lowerStr[0]!==lowerStr[lowerStr.length-1]){
+        return false
+    }
+    return palidrom(lowerStr.slice(1,-1))
+}
+console.log(palidrom('adw'))
+console.log(palidrom('ada'))
 
+// 5)
+function fastMult(a,b){
+    if (b==0){
+        return 1
+    } else if (b/2==0){
+        return fastMult(a*a,b/2)
+    } else if (b/2!==0){
+        return a*fastMult(a,b-1)
+    }
+}
+console.log(fastMult(2,3))
